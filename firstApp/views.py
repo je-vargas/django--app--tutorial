bfce95bd1e01be6ebffb967116bb8fe9bcdec713
juanE <55924277+je-vargas@users.noreply.github.com>
2022-02-22 10:53:21 +0000

    view tweaked

--- firstApp/views.py
+++ firstApp/views.py
@@ -7,15 +7,11 @@ def home(request):
     return HttpResponse("Django you legend")
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%a, %d %b, %Y at %X")
-
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = f"Hello there, {clean_name}! It's {formatted_now}"
-    return HttpResponse(content)
+    return render(
+        request, 
+        "firstApp/hello_there.html",
+    {
+        "name":name,
+        "date": datetime.now()
+    }
+    )
