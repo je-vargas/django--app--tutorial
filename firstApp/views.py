5886f732ee6c901c82baa984d59f5f0bdd32e78d
juanE <55924277+je-vargas@users.noreply.github.com>
2022-02-22 22:26:44 +0000

    views tweaked to include log

--- firstApp/views.py
+++ firstApp/views.py
@@ -2,9 +2,22 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.timezone import datetime
+from django.shortcuts import redirect
+from firstApp.forms import LogMessageForm
+from firstApp.models import LogMessage
+from django.views.generic import ListView
 
-def home(request):
-    return HttpResponse("Django you legend")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+    
+
+# def home(request):
+#     return HttpResponse("Django you legend")
 
 def hello_there(request, name):
     return render(
@@ -24,3 +37,18 @@ def about(request):
 
 def contact(request):
     return render(request, "firstApp/contact.html")
+
+# def log_message(request):
+#     return render(request, "firstApp/log_message.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "firstApp/log_message.html", {"form": form})
